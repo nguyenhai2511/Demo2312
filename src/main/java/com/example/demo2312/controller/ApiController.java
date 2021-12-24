bea0568c864546b206029f7efcb14f9df1f58a8a
gitName <a@b.c>
2021-12-24 13:08:41 +0700

    first commit

--- src/main/java/com/example/demo2312/controller/ApiController.java
+++ src/main/java/com/example/demo2312/controller/ApiController.java
@@ -1,5 +1,6 @@
 package com.example.demo2312.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -7,9 +8,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class ApiController {
 
-    @RequestMapping(value = "/api", method = RequestMethod.GET)
-    public String api (){
-        return "redirect:https://spring-azure-demo-main.azurewebsites.net:443/api/demo0612/triggers/manual/invoke?api-version=2020-05-01-preview&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=tr3gKybxFOImwgGXvYQ34AX6UBzRZYMOXbtrSweyNcg";
+    @RequestMapping(
+            value = "/api",
+            method = RequestMethod.GET,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public String getTest() {
+        return "demo.json";
     }
 
 }
--- src/main/java/com/example/demo2312/controller/HomeController.java
+++ src/main/java/com/example/demo2312/controller/HomeController.java
@@ -6,12 +6,15 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
+import org.springframework.http.MediaType;
 
 @RestController
 public class HomeController {
     private static final String template = "Hello, %s";
     private List<Greeting> list = new ArrayList<>();
     private final AtomicInteger counter = new AtomicInteger();
+
+
 
     @GetMapping("/greeting")
     public List<Greeting> greeting(){
